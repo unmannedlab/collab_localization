0ea394d2b0e1afc7ff9761affc95b9b265ca59b7
Jacob <jacobhartzer@gmail.com>
2020-10-07 12:05:02 -0500

    Transfer

--- matlab/MonteCarlo.m
+++ matlab/MonteCarlo.m
@@ -1,26 +1,26 @@
 clf; clear; close all;
 
-n = 10000;
+n = 10;
 errors_kf_tun_lmk  = zeros(n,3);
 errors_uwb_tun_lmk = zeros(n,3);
 
-parfor i = 1:n
+for i = 1:n
     
     Sim = Simulation('Tunnel',false, [1, 0, 0]);
 
     Sim.run_to(20, 0.1);
-    errors_kf_sc_lmk(i,:) = [   RMSE(Sim.err_out{1}(:,1)), ...
+    errors_kf_tun_lmk(i,:) = [   RMSE(Sim.err_out{1}(:,1)), ...
                                 RMSE(Sim.err_out{1}(:,2)), ...
                                 RMSE(Sim.err_out{1}(:,3))];
 
 end
 
-parfor i = 1:n
+for i = 1:n
     
     Sim = Simulation('Tunnel',false, [1, 0, 1]);
     
     Sim.run_to(20, 0.1);
-    errors_uwb_sc_lmk(i,:) = [  RMSE(Sim.err_out{1}(:,1)), ...
+    errors_uwb_tun_lmk(i,:) = [  RMSE(Sim.err_out{1}(:,1)), ...
                                 RMSE(Sim.err_out{1}(:,2)), ...
                                 RMSE(Sim.err_out{1}(:,3))];
 
@@ -28,9 +28,9 @@ end
 
 %%
 
-h1 = histogram(errors_kf_lmk(:,1),50,'Normalization','probability','EdgeColor','none');
+h1 = histogram(errors_kf_tun_lmk(:,1),50,'Normalization','probability','EdgeColor','none');
 hold on;
-h2 = histogram(errors_uwb_lmk(:,1),50,'Normalization','probability','EdgeColor','none');
+h2 = histogram(errors_uwb_tun_lmk(:,1),50,'Normalization','probability','EdgeColor','none');
 legend('KF - No UWB', 'KF - With UWB');
 xlabel('RMSE [m]')
 ylabel('Frequency')
