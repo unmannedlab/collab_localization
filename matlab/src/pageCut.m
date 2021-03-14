43400dd7b43284380255aa27e7ab7beed64ce2fe
Jacob <JacobHartzer@gmail.com>
2021-03-14 17:27:06 -0500

    Removing extra src

--- matlab/src/pageCut.m
+++
@@ -1,8 +0,0 @@
-function [out] = pageCut(in)
-    out = zeros(size(in));
-    for i = 1:size(in,3)
-        t = tril(in(:,:,i));
-        out(:,:,i) = t + t' - diag(diag(in(:,:,i)));
-    end
-end
-
